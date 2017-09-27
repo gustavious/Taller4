b250898d6497cd533d8a6c8636470c3145c02dc5
Diego Florez <diego.florez@eurekadigital.co>
2017-09-27 01:29:38 -0500

    Taller 6

--- TablaPlatos/PedidoViewController.swift
+++ TablaPlatos/PedidoViewController.swift
@@ -8,22 +8,39 @@
 
 import UIKit
 import AFNetworking
+import CoreLocation
+import CoreMotion
 
 var metodoSeleccionado: String = "Efectivo"
 
-class PedidoViewController: UIViewController {
+class PedidoViewController: UIViewController, CLLocationManagerDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var locationManager: CLLocationManager = CLLocationManager()
+    var userLocation: CLLocation!
+    var motionManager: CMMotionManager!
     
     var platoSeleccionado: Plato!
     @IBOutlet weak var lugarTextField: UITextField!
     @IBOutlet weak var nombreTextField: UITextField!
-    
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var imagePreview: UIImageView!
     @IBOutlet weak var labelMetodoPago: UILabel!
     
+    @IBOutlet weak var labelInfoAcelerometro: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print("ViewDidLoad")
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        userLocation = nil
+        
+        motionManager = CMMotionManager()
+        motionManager.startAccelerometerUpdates()
 
         // Do any additional setup after loading the view.
     }
@@ -32,6 +49,58 @@ class PedidoViewController: UIViewController {
         super.viewWillAppear(animated)
         labelMetodoPago.text = "Pago con " + metodoSeleccionado
         print("view will appear")
+    }
+    
+    
+    @IBAction func tomarFoto(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera){
+            let imagePicker = UIImagePickerController()
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera
+            imagePicker.mediaTypes = [kUTTypeImage as String]
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let mediaType = info[UIImagePickerControllerMediaType] as! NSString
+        
+        self.dismiss(animated: true, completion: nil)
+        
+        if mediaType.isEqual(to: kUTTypeImage as String){
+            let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+            
+            imagePreview.image = image
+        }
+        
+        
+    }
+    
+    
+    @IBAction func seleccionarFoto(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.savedPhotosAlbum){
+            let imagePicker = UIImagePickerController()
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+            imagePicker.mediaTypes = [kUTTypeImage as String]
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    
+    @IBAction func obtenerInfoAcelerometro(_ sender: Any) {
+        
+        if let accelerometerData = motionManager.accelerometerData{
+            labelInfoAcelerometro.text = "X:" + String(accelerometerData.acceleration.x) +
+        " Y:" + String(accelerometerData.acceleration.y) +
+            " Z:" + String(accelerometerData.acceleration.z)
+        }
+        
     }
     
     
@@ -99,6 +168,21 @@ class PedidoViewController: UIViewController {
         self.present(alertController, animated: true){
             
         }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let latestLocation: CLLocation = locations[locations.count - 1]
+        
+        if userLocation == nil{
+            userLocation = latestLocation
+        }
+        
+        print(String(userLocation.coordinate.latitude) + ", " + String(userLocation.coordinate.longitude))
+        locationLabel.text = "Ubicacion: " + String(userLocation.coordinate.latitude) + ", " + String(userLocation.coordinate.longitude)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Locarion Manager Error: " + error.localizedDescription)
     }
     
 
--- TablaPlatos/ViewController.swift
+++ TablaPlatos/ViewController.swift
@@ -8,19 +8,26 @@
 
 import UIKit
 import AFNetworking
+import Contacts
+import ContactsUI
+import MessageUI
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, CNContactPickerDelegate, MFMessageComposeViewControllerDelegate {
     
     @IBOutlet weak var tableViewPlatos: UITableView!
+   
     var platos:[Plato] = [Plato]()
+    let contactPicker: CNContactPickerViewController = CNContactPickerViewController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print ("Llega hasta aqui")
+       
         // Do any additional setup after loading the view, typically from a nib.
         tableViewPlatos.delegate = self
         tableViewPlatos.dataSource = self
+        //contactPicker.delegate = self
+        
         
        getPlatos()
     }
@@ -28,6 +35,69 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+  
+    
+    @IBAction func compartirPlato(_ sender: Any) {
+        self.present(contactPicker, animated:true, completion: nil)
+    }
+    
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        switch result {
+        case .sent:
+            print("Message sent:")
+        case .failed:
+            print("Message failes")
+        case .cancelled:
+            print("Message Cancelled")
+        }
+    }
+    
+    func sendSMS(contact: CNContact){
+        if !MFMessageComposeViewController.canSendText(){
+            print("No es posible enviar mensajes en este dispositivo")
+            return
+        }
+        
+        if contact.phoneNumbers.count > 0{
+            
+            let recipents: [String] = [contact.phoneNumbers[0].value.stringValue, "3166420466"]
+            let messageController: MFMessageComposeViewController = MFMessageComposeViewController()
+            messageController.messageComposeDelegate = self
+            messageController.recipients = recipents
+            messageController.body = "Mensaje de prueba"
+            
+            self.present(messageController, animated: true, completion: nil)
+        }
+        else {
+            print("El contacto no tiene numeros telefonicos")
+        }
+    }
+    
+    func contactPicker(_ picker: CNContactPickerViewController, didSelect contact: CNContact) {
+        picker.dismiss(animated:true){
+         self.sendSMS(contact: contact)
+        }
+    }
+    
+    @IBAction func compartirPlatoWhatsApp(_ sender: Any) {
+        let msg = "Mensaje de prueba"
+        let urlWhats = "whatsapp://send?text=\(msg)"
+        if let urlString = urlWhats.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed){
+            
+            if let whatsappURL = URL(string: urlString){
+                if UIApplication.shared.canOpenURL(whatsappURL){
+                    UIApplication.shared.open(whatsappURL, options: [:], completionHandler: { (completed: Bool) in
+                        
+                    })
+                }
+                else{
+                    print("No se puede abrir porque no esta intalado")
+                }
+            }
+           
+        }
     }
     
     func getPlatos(){
